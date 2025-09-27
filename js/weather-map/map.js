661e32dc078b2339d69b9764f217576fcde0b93f
TheDevinNikitenko <103870601+TheDevinNikitenko@users.noreply.github.com>
2025-09-26 20:58:18 -0400

    Delete js directory

--- js/weather-map/map.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map', {
-    center: [25.774902898965, -80.1861348388672],
-    minZoom: 2,
-    zoom: 10.25
-});
-L.tileLayer( 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
-   
-}).addTo( map );
